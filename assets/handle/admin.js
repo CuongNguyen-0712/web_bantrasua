07cf746b9e53af7466eea9d81c36ed4bfb4a3aea
CuongCoder <nguyenquoccuong07122004@gmail.com>
2024-10-21 21:01:12 +0700

    create-admin

---
+++ assets/handle/admin.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchBtn = document.querySelector(".search_icon");
+    const searchBar = document.querySelector(".search");
+    const inputValueSearch = document.querySelector(".input_search");
+
+    searchBtn.addEventListener("click", () => {
+        searchBar.classList.toggle("active");
+        if(searchBar.classList.contains("active")){
+            inputValueSearch.focus();
+        }
+        else{
+            inputValueSearch.value = "";
+        }
+    });
+
+});
