e6fc6469b497ee58d2c1353b3dfb10f4caab558c
Zair0u <quynhtien123123@gmail.com>
2024-12-10 12:21:30 +0700

    done

--- assets/handle/user/loadIframe.js
+++ assets/handle/user/loadIframe.js
@@ -2,3 +2,8 @@ const handleLoadContent = (page) => {
     var iframe = document.getElementById('content_user');
     iframe.src = page;
 }
+
+function check() {
+    alert("Vui lòng đăng nhập để tiếp tục!!!");
+    document.location.href = '/pages/user/login.html'
+}
